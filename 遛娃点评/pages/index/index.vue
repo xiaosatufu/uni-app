35baf144a1cd236c9979058ac2185446c59cb008
yongshuang <xiaosatufu@163.com>
2020-06-12 18:39:43 +0800

    lwdp

---
+++ 遛娃点评/pages/index/index.vue
@@ -0,0 +1,76 @@
+<template>
+	<view class="content">
+		<view class="m-header" :style="'height:' +  header.height + 'px;' + 'padding-top:' + header.top + 'px;'">
+			<view class="u-address">
+				<text class="u-text">成都</text>
+				<view class="u-arrow"></view>
+			</view>
+			<view class="u-logo">
+				<image class="u-img" src="../../static/images/lwdp_logo@3x.png" mode=""></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				header: {
+					top: 0,
+					height: 0
+				}
+			}
+		},
+		onLoad() {
+
+		},
+		created() {
+			const header = uni.getMenuButtonBoundingClientRect()
+			this.header.top = header.top
+			this.header.height = header.height
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.m-header {
+		background: #F85D25;
+		width: 100%;
+		display: flex;
+		// justify-content: center;
+		align-items: center;
+		position: relative;
+
+		.u-address {
+			color: #fff;
+			font-size: 36rpx;
+			margin-left: 32rpx;
+			display: flex;
+			align-items: center;
+			.u-arrow {
+				width: 40rpx;
+				height: 40rpx;
+				background: url(../../static/images/icon_home_arrow_lower@3x.png);
+				background-size: 100%;
+				margin-left: 8rpx;
+			}
+		}
+
+		.u-logo {
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			.u-img {
+
+				width: 144rpx;
+				height: 44rpx;
+			}
+
+		}
+	}
+</style>
---
+++ 遛娃点评/pages/login/login.vue
@@ -0,0 +1,177 @@
+<template>
+	<view class="app-container">
+		<view class="main-title">
+			欢迎来到遛娃点评
+		</view>
+		<view class="login-form">
+			<form>
+				<view class="uni-form-item uni-column">
+					<view class="title">手机号码</view>
+					<input class="uni-input" placeholder-class="font-color" v-model="form.mobile" placeholder="请输入您的手机号码" />
+				</view>
+				<view class="uni-form-item uni-column">
+					<view class="title">获取验证码</view>
+					<div class="m-code">
+						<input class="uni-input" placeholder-class="font-color" v-model="form.smscode" placeholder="请输入验证码" />
+						<view class="u-divide"></view>
+						<div class="u-get-code">获取验证码</div>
+					</div>
+				</view>
+				<view class="uni-btn-v">
+
+					<button class="login-btn normal-login-btn active" @click="handlelogin('normal')">登录</button>
+					<view class="login-btn wx-login-btn">
+						<view class="icon-wx"></view>
+						<text class="u-text">
+							微信快捷登录
+						</text>
+					</view>
+				</view>
+			</form>
+		</view>
+		<view class="u-label">点击登录按钮代表您已同意用户协议和政策隐私</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					mobile: '',
+					smscode: ''
+				}
+			};
+		},
+		methods: {
+			handlelogin(type) {
+				console.log(type)
+				console.log(this.form)
+				uni.redirectTo({
+					url: '../index/index'
+				});
+			},
+
+			formSubmit: function(e) {
+				console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
+				var formdata = e.detail.value
+				uni.showModal({
+					content: '表单数据内容：' + JSON.stringify(formdata),
+					showCancel: false
+				});
+			},
+			formReset: function(e) {
+				console.log('清空数据')
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.font-color {
+		color: #B3B3B3;
+		font-size: 24rpx;
+	}
+
+	.app-container {
+		margin: 0 60rpx;
+
+		.main-title {
+			padding: 92rpx 0 20rpx 0;
+
+			font-size: 52rpx;
+			color: #222222;
+			font-weight: 600;
+
+		}
+
+		.login-form {
+			.uni-form-item {
+				.title {
+					font-size: 32rpx;
+					color: #222;
+					font-weight: 500;
+					margin-top: 60rpx;
+				}
+
+				.m-code {
+					display: flex;
+					align-items: center;
+					position: relative;
+					border-bottom: solid 1rpx #F2F2F2;
+
+					.uni-input {
+						flex: 1;
+						border: none;
+					}
+
+					.u-get-code {
+						width: 173rpx;
+						text-align: right;
+						font-size: 28rpx;
+						color: #555;
+					}
+
+					.u-divide {
+						width: 2rpx;
+						height: 32rpx;
+						background: #f2f2f2;
+					}
+				}
+
+				.uni-input {
+					height: 100rpx;
+					border-bottom: solid 1rpx #F2F2F2;
+
+				}
+			}
+
+			.uni-btn-v {
+				margin-top: 120rpx;
+
+				.login-btn {
+					width: 630rpx;
+					height: 100rpx;
+					line-height: 100rpx;
+					border-radius: 50rpx;
+					font-size: 36rpx;
+					color: #FFFFFF;
+
+					&.normal-login-btn {
+						background: linear-gradient(138deg, rgba(253, 118, 62, 1) 0%, rgba(241, 59, 3, 1) 100%);
+						opacity: 0.5;
+						margin-bottom: 60rpx;
+
+						&.active {
+							opacity: 1;
+						}
+					}
+
+					&.wx-login-btn {
+						background: linear-gradient(135deg, rgba(54, 187, 99, 1) 0%, rgba(70, 187, 54, 1) 100%);
+						display: flex;
+						align-items: center;
+						justify-content: center;
+
+						.icon-wx {
+							width: 46rpx;
+							height: 40rpx;
+							background: url(../../static/images/icon_wechat@3x.png);
+							background-size: 100%;
+							margin-right: 24rpx;
+						}
+					}
+				}
+			}
+		}
+
+		.u-label {
+			font-size: 24rpx;
+			color: #B3B3B3;
+			text-align: center;
+			position: absolute;
+			bottom: 120rpx;
+			width: 630rpx;
+		}
+	}
+</style>
